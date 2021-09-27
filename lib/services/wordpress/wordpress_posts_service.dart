c9a27fabc7c76ba7612294bcb456bcfdc2813fd5
Brian Lu <lu_haoqin@outlook.com>
2021-09-26 22:42:53 -0500

    added cache for individual pages

--- lib/services/wordpress/wordpress_posts_service.dart
+++ lib/services/wordpress/wordpress_posts_service.dart
@@ -11,6 +11,8 @@ class WordpressPostService {
 
   static final DateFormat dateFormat = DateFormat("yyyy-MM-ddThh:mm:ss");
 
+  // Caches are references to the same objects TODO test if they actually are
+  static Map<int, PostModel> postCache = {};
   static Map<int, ListPage<PostModel>> pageCache = {};
 
   static void init(WordPressAPI api) {
@@ -24,7 +26,7 @@ class WordpressPostService {
         log.info("Cache hit!");
       } else {
         final WPResponse res =
-        await WordpressPostService.api!.fetch('posts/', args: {
+            await WordpressPostService.api!.fetch('posts/', args: {
           "page": page,
           "per_page": 10,
         });
@@ -32,8 +34,9 @@ class WordpressPostService {
         List<PostModel> posts = _blacklistPosts(res);
         pageCache[page] = new ListPage<PostModel>(posts, res.data.length < 10);
         log.info(
-            "Fetched ${res.data.length} posts from API. (${posts
-                .length} after blacklist)");
+            "Fetched ${res.data.length} posts from API. (${posts.length} after blacklist)");
+        // Also add the posts we got into the post cache
+        for (PostModel post in posts) postCache[post.id] = post;
       }
       return pageCache[page]!;
     } catch (e) {
@@ -42,40 +45,53 @@ class WordpressPostService {
     }
   }
 
+  static Future<PostModel> getPost(int postId) async {
+    if (!postCache.containsKey(postId)) {
+      final WPResponse res =
+      await WordpressPostService.api!.fetch('posts/$postId');
+      postCache[postId] = _postFromMap(res.data);
+    }
+    return postCache[postId]!;
+  }
+
   static void clearCache() {
+    postCache = {};
     pageCache = {};
   }
 
   static List<PostModel> _blacklistPosts(WPResponse res) {
-    final unescape = HtmlUnescape();
     List<PostModel> posts = List.empty(growable: true);
     for (final post in res.data) {
       // Temporary fix for posts with invalid pages/formatting
-      // TODO add post preprocessing service
+      // TODO add featured content support
       // Blacklist posts with video category (ID 123)
       // Blacklist posts with no pages
-
-      // We also have to interpret data as a map since we fetch with a modified
-      // API, so we can't use the wordpress_api package utility class.
       if (!(List<int>.from(post["categories"] ?? []).contains(123) ||
-          post["content"] == ""))
-        posts.add(new PostModel(
-            post["id"],
-            dateFormat.parse(post["date_gmt"], true).toLocal(),
-            dateFormat.parse(post["modified_gmt"], true).toLocal(),
-            post['guid']?['rendered'],
-            post["slug"],
-            post["link"],
-            unescape.convert(post['title']?['rendered']),
-            post['content']?['rendered'],
-            post["ct_raw"],
-            post['excerpt']?['rendered'],
-            post["author"],
-            post["featured_media"],
-            List<int>.from(post["categories"] ?? []),
-            List<int>.from(post["tags"] ?? []),
-            post["comment_status"]));
+          post["content"].toString().trim() == ""))
+        posts.add(_postFromMap(post));
     }
     return posts;
+  }
+
+  static PostModel _postFromMap(Map<String, dynamic> postMap) {
+    // We also have to interpret data as a map since we fetch with a modified
+    // API, so we can't use the wordpress_api package utility class.
+    final unescape = HtmlUnescape();
+    return new PostModel(
+        postMap["id"],
+        dateFormat.parse(postMap["date_gmt"], true).toLocal(),
+        dateFormat.parse(postMap["modified_gmt"], true).toLocal(),
+        postMap['guid']?['rendered'],
+        postMap["slug"],
+        postMap["link"],
+        unescape.convert(postMap['title']?['rendered']),
+        postMap['content']?['rendered'],
+        postMap["ct_raw"],
+        postMap['excerpt']?['rendered'],
+        postMap["author"],
+        postMap["featured_media"],
+        List<int>.from(postMap["categories"] ?? []),
+        List<int>.from(postMap["tags"] ?? []),
+        postMap["comment_status"]);
   }
 }
